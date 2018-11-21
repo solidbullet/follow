8d2b12fb51c9bf194fcc0045b703caa655d5290d
solidbullet <691622093@qq.com>
2018-11-21 14:29:13 +0800

    update auth 1

--- auth/server.js
+++ auth/server.js
@@ -2,34 +2,34 @@
 const http =require("http");
 var URL = require('url');
 const redis = require('redis')
-//引入文件模块
+
+var util = require('util');
 const fs= require("fs");
 //创建服务器
 const server = http.createServer(function(req,res){
-
+	const client = redis.createClient(6379, '127.0.0.1')
 	end = req.url.indexOf("?");
 	//console.log(req.url.slice(0,end));
-	res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"})
+	res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8",'Access-Control-Allow-Origin':'hiiboy.com:8888'})
 	//请求的路由地址
 	if(req.url == "/" || req.url=="/index.html"){
 		
 		fs.readFile("index.html",function(err,data){
-			//设置响应头
-			res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
 			//加载的数据结束
 			res.end(data)
 		})
 	}else if(req.url == "/auth" || req.url=="/draw.html"){
 			fs.readFile("auth.html",function(err,data){
 				//设置响应头
-				res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
 				//加载的数据结束
 				res.end(data)
 			})
+	}else if(req.url == "/redis"){
+		client.hgetall("user", function (err, obj) {	
+			res.end(JSON.stringify(obj));
+		});	
 	}else if(req.url.slice(0,end) == "/save"){
 		var arg = URL.parse(req.url,true).query;  //方法二arg => { account: '001', auth: '1' }
-		//console.log(arg);
-		const client = redis.createClient(6379, '127.0.0.1')
 		/*
 		client.set('hello', JSON.stringify(arg)) // 注意，value会被转为字符串,所以存的时候要先把value 转为json字符串
 		client.get('hello', function(err, value){
@@ -39,14 +39,10 @@ const server = http.createServer(function(req,res){
 		client.hset('user', arg.account,arg.auth, function(data) {
 			  console.log(arg.account,"  ",arg.auth)
 		})
-		//client.hset("hash key", "field 1", "v1", redis.print);
-		    res.writeHead(200,{
-       					 'Access-Control-Allow-Origin':'hiiboy.com:8888'
-   				 });
+		//res.writeHead(200,{'Access-Control-Allow-Origin':'hiiboy.com:8888'});
 		res.end("save");
 				
 	}else{
-		res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
 			//加载的数据结束
 			res.end('<h1> 所需内容未找到404 </h1>')
 	}

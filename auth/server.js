8f97e8ff5546f7e50fe8850df552b31e73aac36c
solidbullet <691622093@qq.com>
2018-12-10 16:39:02 +0800

    update server.sj

--- auth/server.js
+++ auth/server.js
@@ -2,13 +2,14 @@
 const http =require("http");
 var URL = require('url');
 const redis = require('redis')
-
+var request = require('request');
+var crypto = require('crypto');
 var util = require('util');
 const fs= require("fs");
 //创建服务器
 const server = http.createServer(function(req,res){
 	const client = redis.createClient(6379, '127.0.0.1');
-	client.auth('pass',function(err, reply) {
+	client.auth('810302',function(err, reply) {
 	 console.log(reply);
 	});
 	end = req.url.indexOf("?");
@@ -39,11 +40,24 @@ const server = http.createServer(function(req,res){
 		client.hgetall("user", function (err, obj) {	
 			res.end(JSON.stringify(obj));
 		});	
-	}else if(req.url == "/show"){
-			fs.readFile("show.html",function(err,data){
-				//设置响应头
-				res.end(data)
-			})
+	}else if(req.url == "/bitmex"){
+			if (req.method === 'POST') {
+				var body = '';
+				req.on('data', chunk => {
+					body += chunk.toString(); // convert Buffer to string
+				});
+				req.on('end', () => {
+					console.log(body);
+					//var postBody = JSON.stringify(body);
+					
+					bitmex(body);
+					res.end('ok');
+				});
+			}
+			//var data = {symbol:"XBTUSD",orderQty:2,ordType:"Market"};
+			//var postBody = JSON.stringify(data);
+			//bitmex(postBody);
+			res.end("open success");
 	}else if(req.url.slice(0,end) == "/save"){
 		var arg = URL.parse(req.url,true).query;  //方法二arg => { account: '001', auth: '1' }
 		/*
@@ -64,3 +78,42 @@ const server = http.createServer(function(req,res){
 	}
 
 }).listen(8888)
+
+function bitmex(postBody)
+{
+var apiKey = "Ol8zB3C1KtB7IGeMp1i6Y1si";
+var apiSecret = "aKMNONbyhZavg8xwSsB9DzO3_3b1oruJdQrcSh9N9nNI3T97";
+
+var verb = 'POST',
+  path = '/api/v1/order',
+  expires = new Date().getTime() + (60 * 1000); // 1 min in the future
+  //data = {symbol:"XBTUSD",orderQty:1,price:590,ordType:"Limit"};
+
+
+var signature = crypto.createHmac('sha256', apiSecret).update(verb + path + expires + postBody).digest('hex');
+
+var headers = {
+  'content-type' : 'application/json',
+  'Accept': 'application/json',
+  'X-Requested-With': 'XMLHttpRequest',
+  // This example uses the 'expires' scheme. You can also use the 'nonce' scheme. See
+  // https://www.bitmex.com/app/apiKeysUsage for more details.
+  'api-expires': expires,
+  'api-key': apiKey,
+  'api-signature': signature
+};
+
+const requestOptions = {
+  headers: headers,
+  url:'https://testnet.bitmex.com'+path,
+  method: verb,
+  body: postBody,
+  proxy:'http://127.0.0.1:1080'
+};
+
+ request(requestOptions, function(error, response, body) {
+   if (error) { console.log(error); }
+   console.log(body);
+ });
+
+}
